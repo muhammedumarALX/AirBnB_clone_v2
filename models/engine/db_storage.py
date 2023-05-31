9f25e84c9e709e9ee96069d763810a6971733e61
muhammed umar <muhammedumaralx@gmail.com>
2023-05-31 22:56:13 +0100

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -51,4 +51,6 @@ class DBStorage:
     def reload(self):
         """Reload a new database session"""
         Base.metadata.create_all(self.__engine)
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        self.__session = scoped_session(session_factory)
+
