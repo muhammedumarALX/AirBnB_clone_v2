1c05b35cb989b87f6b2acffddbc91fa5a5f822b5
muhammed umar <muhammedumaralx@gmail.com>
2023-06-07 23:23:50 +0100

    task 7

--- models/user.py
+++ models/user.py
@@ -5,7 +5,14 @@ from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This class defines a user by various attributes
+        Attributes:
+            __tablename__ (str): MySQl table to store the user
+            email (sqlalchemy.string): The user email
+            password (sqlalchemy.string): The user password
+            first_name (sqlalchemy.string): The user first name
+            last_name (sqlalchemy.string): The user last name
+    """
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
